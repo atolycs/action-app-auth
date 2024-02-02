28fc44a80bfb8e8de2fa5cd58edfcc40863555a2
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-02-02 16:48:15 +0900

    Test App Infomation get

--- src/main.js
+++ src/main.js
@@ -8,11 +8,13 @@ async function run() {
     const privateKey = core.getInput('privateKey', { required: true });
     //const repo_info = process.env.GITHUB_REPOSITORY
 
-    const codeOwner = process.env.GITHUB_REPOSITORY_OWNER;
+    //const codeOwner = process.env.GITHUB_REPOSITORY_OWNER;
     //const codeOwner = repo_info.split("/")[0]
     //const repo_name = repo_info.split("/")[1]
 
     core.info('==> Setup Token...');
+
+    // Create App Authentication
 
     const auth = createAppAuth({
       appId: appID,
@@ -20,51 +22,19 @@ async function run() {
       request: request.defaults(),
     });
 
-    core.debug('===> auth setup.');
+    core.debug('===> Auth setup.');
 
     const auth_app_token = await auth({
       type: 'app',
     });
-
-    core.info('==> User Installation ID Getting ');
-    let authentication = await request('GET /users/{username}/installation', {
-      username: codeOwner,
-      request: {
-        hook: auth.hook,
-        headers: {
-          authentication: `token ${auth_app_token.token}`,
-        },
+    core.info(`App Token Generated: ${auth_app_token.token}`);
+    core.info(`==> Bot user infomation setup`);
+    await request('GET /app', {
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+        authorization: auth_app_token.token,
       },
     });
-
-    core.setSecret(authentication.data.id);
-    core.info(`==> Get User Installation ID.`);
-    core.info(authentication.data.id);
-    core.info(`==> Getting Bot commit User data...`);
-    const user_info = await request(`GET /user`, {
-      request: {
-        hook: auth.hook,
-        headers: {
-          authentication: `token ${auth_token.token}`,
-        },
-      },
-    });
-
-    core.info(user_info)
-
-    const auth_token = auth({
-      type: 'installation',
-      installationId: authentication.data.id,
-    });
-
-    /*const auth_token = await auth({
-      type: 'installation',
-      installationId: authentication.data.id,
-    });*/
-
-    //core.setSecret(auth_token.token);
-    //core.setOutput("token", auth_token.token)
-    console.log(user_info);
   } catch (error) {
     core.setFailed(error);
   }
