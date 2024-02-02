2e0eb5169125b6bea03244d909ee50e6c4312954
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-02-02 17:27:51 +0900

    Fix

--- src/main.js
+++ src/main.js
@@ -60,6 +60,8 @@ async function run() {
 
     core.info('==> Getting Bot user data');
     let app_info = await octokit.request('GET /app');
+
+    core.info(`${app_info.id}`)
     core.info(`==> ${app_info}`)
     const bot_name = `${app_info.slug}[bot]`;
     core.info(bot_name)
