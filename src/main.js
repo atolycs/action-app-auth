f4ebdde09abbc8b2e0648c93348ab2f92a87257c
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-02-02 13:37:25 +0900

    Add APP ACTION

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const { Octokit } = require('@octokit/rest');
+const { createAppAuth } = require('@octokit/auth-app');
+
+async function run() {
+  const appID = core.getInput('appID', { required: true });
+  const privateKey = core.getInput('privateKey', { required: true });
+
+  const appOctokit = new Octokit({
+    authStrategy: createAppAuth,
+    auth: {
+      appId: appID,
+      privateKey: privateKey,
+    },
+  });
+
+  const { data } = await appOctokit.request('/app');
+  /* const { token } = await appOctokit.auth({
+        type: "isntallation"
+    })*/
+
+  console.log(data);
+}
+
+run();
